ac9eafe1bbead5f2e9a17d84bdda6a2d3e1160ed
Tomek <tomasz.ciecwierz@gmail.com>
2018-04-20 15:35:12 +0200

    firstcommit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String tekst1 = "domeek";
+        String tekst2 = "doemek";
+        boolean isAnagram = true;
+
+        List<Character> listOfChar = new ArrayList<Character>();
+
+        if (tekst1.length() != tekst2.length()) {
+            isAnagram = false;
+        }
+        for (int i = 0; i < tekst1.length(); i++) {
+            listOfChar.add(tekst1.charAt(i));
+        }
+
+        for (int i = 0; i < tekst2.length(); i++) {
+            if (listOfChar.contains(tekst2.charAt(i))) {
+                listOfChar.remove(listOfChar.indexOf(tekst2.charAt(i)));
+            } else {
+                isAnagram = false;
+            }
+        }
+        System.out.println("Anagram: " + isAnagram);
+    }
+}
